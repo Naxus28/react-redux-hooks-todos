6a2d186e7d7bf9d50d1ba8abfdc7f1148c9307f9
Gabriel Ferraz <gabrielferraz27@gmail.com>
2019-10-21 17:46:08 -0400

    Adds styles

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,7 +2,10 @@ import React from "react";
 import cn from "classnames";
 
 const Todo = ({ todo, toggleTodo }) => (
-  <li className={cn({ completed: todo.active }, 'todo')} onClick={() => toggleTodo(todo.id)}>
+  <li
+    className={cn({ completed: todo.active }, "todo")}
+    onClick={() => toggleTodo(todo.id)}
+  >
     {todo.item}
   </li>
 );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import Todo from './Todo';
+import React from "react";
+import Todo from "./Todo";
 
 const TodoList = ({ todos, toggleTodo }) => {
   return (
     <div className="todo-list">
-      {todos.length 
-        ? todos.map(todo => <Todo todo={todo} toggleTodo={toggleTodo} key={todo.id} /> ) 
-        : <p>No todos yet :)</p>
-      }
+      {todos.length ? (
+        <ul>
+          {todos.map(todo => (
+            <Todo todo={todo} toggleTodo={toggleTodo} key={todo.id} />
+          ))}
+        </ul>
+      ) : (
+        <p>No todos yet :)</p>
+      )}
     </div>
   );
 };
-
-
 
 export default TodoList;
